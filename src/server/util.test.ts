be548007d1c16f3b022400c6cfce104b2e8a2c0c
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-12-05 09:12:43 +0600

    feat(server): add custom queryParser middleware

--- src/server/util.test.ts
+++ src/server/util.test.ts
@@ -1,5 +1,5 @@
 import { deepFreeze } from "../common/util/deep-freeze";
-import { parseQuery } from "./util";
+import { makeExpressQueryParserMiddleware, parseQuery } from "./util";
 
 describe("parseQuery", () => {
   it(`parses a base64 encoded json object`, () => {
@@ -14,5 +14,93 @@ describe("parseQuery", () => {
 
     const parsedQueryObject = parseQuery({ query });
     expect(parsedQueryObject).toEqual(object);
+  });
+});
+
+describe("makeExpressQueryParserMiddleware", () => {
+  const errorBody = deepFreeze({
+    success: false,
+    errorType: "msgAndCode",
+    error: { message: "Invalid query" },
+  } as const);
+  const queryFieldName = "q";
+
+  const parseQuery = jest.fn();
+  const res = Object.freeze({
+    status: jest.fn(),
+    json: jest.fn(),
+  });
+  const next = jest.fn();
+
+  beforeEach(() => {
+    parseQuery.mockReset();
+    next.mockReset(),
+      Object.values(res).forEach((method) => method.mockReset());
+  });
+
+  const expressQueryParserMiddleware = makeExpressQueryParserMiddleware({
+    errorBody,
+    parseQuery,
+    queryFieldName,
+  });
+
+  it.each([
+    {
+      req: { query: {} },
+      case: "missing queryFieldName in query object",
+      beforeTest: () => {},
+    },
+    {
+      req: { query: { [queryFieldName]: "abc" } },
+      case: "the parsed query is an array",
+      beforeTest: () => {
+        parseQuery.mockReturnValue([]);
+      },
+    },
+
+    {
+      req: { query: { [queryFieldName]: "abc" } },
+      case: "the parseQuery function throws error",
+      beforeTest: () => {
+        parseQuery.mockImplementationOnce(() => {
+          throw new Error("Invalid query");
+        });
+      },
+    },
+  ])(`sends error response if $case`, ({ req, beforeTest }) => {
+    beforeTest();
+
+    expressQueryParserMiddleware(<any>req, <any>res, next);
+
+    if (queryFieldName in req.query)
+      expect(parseQuery).toHaveBeenCalledWith({
+        query: req.query[queryFieldName],
+      });
+
+    expect(next).not.toHaveBeenCalled();
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledTimes(1);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith(errorBody);
+  });
+
+  it(`assigns the queryObject to req.query and calls next`, () => {
+    const fakeParsedQueryObject = deepFreeze({ a: 1, b: { c: 3 } });
+    parseQuery.mockReturnValueOnce(fakeParsedQueryObject);
+
+    const query = deepFreeze({ [queryFieldName]: "abc" });
+    const req = { query };
+
+    expressQueryParserMiddleware(<any>req, <any>res, next);
+
+    expect(parseQuery).toHaveBeenCalledWith({ query: query[queryFieldName] });
+    expect(req.query).toEqual(fakeParsedQueryObject);
+
+    expect(next).toHaveBeenCalledTimes(1);
+
+    expect(res.status).not.toHaveBeenCalled();
+    expect(res.json).not.toHaveBeenCalled();
   });
 });
--- src/server/util.ts
+++ src/server/util.ts
@@ -1,7 +1,12 @@
-export interface ParseQuery_Argument {
-  query: string;
-}
-export function parseQuery(arg: ParseQuery_Argument): object {
+import type { RequestHandler } from "express";
+import { HttpResponse } from "../controllers/interface";
+
+type ParseQuery = (arg: { query: string }) => object;
+
+/**
+ * Parses urlEncoded base64 json string.
+ */
+export const parseQuery: ParseQuery = function _parseQuery(arg): object {
   const { query } = arg;
 
   const jsonQuery = Buffer.from(decodeURIComponent(query), "base64").toString(
@@ -9,4 +14,41 @@ export function parseQuery(arg: ParseQuery_Argument): object {
   );
 
   return JSON.parse(jsonQuery);
+};
+
+export interface MakeExpressQueryParserMiddleware_Argument {
+  parseQuery: ParseQuery;
+  queryFieldName: string;
+  errorBody: HttpResponse["body"];
+}
+
+/**
+ * Parses the `req.query[queryFieldName]` field then assigns
+ * the parsed object to `req.query` and calls `next()`
+ * */
+export function makeExpressQueryParserMiddleware(
+  factoryArg: MakeExpressQueryParserMiddleware_Argument
+): RequestHandler {
+  const { parseQuery, queryFieldName, errorBody } = factoryArg;
+
+  return (req, res, next) => {
+    const sendErrorResponse = () => {
+      res.status(400);
+      res.json(errorBody);
+    };
+
+    if (!(queryFieldName in req.query)) return sendErrorResponse();
+
+    try {
+      const query = String(req.query[queryFieldName]);
+      const queryObject = parseQuery({ query });
+
+      if (Array.isArray(queryObject)) return sendErrorResponse();
+
+      req.query = queryObject as any;
+      next();
+    } catch (ex) {
+      sendErrorResponse();
+    }
+  };
 }
