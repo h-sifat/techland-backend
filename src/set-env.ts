6eabc8986c04b90ca8032bf158b214ffc9ba6433
Sifat Hossain <80267830+h-sifat@users.noreply.github.com>
2022-12-05 14:18:05 +0600

    feat: add set-env to load environment variables

---
+++ src/set-env.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const variables = Object.freeze([
+  "PORT",
+  "DB_NAME",
+  "DB_HOST",
+  "DB_USERNAME",
+  "DB_PASSWORD",
+  "DB_PROTOCOL",
+  "IMAGE_URL_PREFIX",
+  "PRODUCTS_COL_NAME",
+  "PRODUCT_CATEGORIES_COL_NAME",
+] as const);
+
+const changes = variables.reduce((changes, variable) => {
+  changes[variable] = process.env[variable]!;
+  return changes;
+}, {} as Record<string, string>);
+
+import { modifyConfig } from "./config";
+modifyConfig({ changes });
